9d83d141bb3c3bbf155bc79d24a32669e91cfaeb
shahriarbsc <shahriar.bsc@gmail.com>
2023-03-26 23:37:18 -0400

    1st commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Hw:
+        //Create a repo on local, have a basic code in it for printing integers from 1 to 10
+        //Push it to github.
+
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
